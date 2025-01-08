7d6a3078e187c1cad7cd118c4913b90fe288faa7
melfakir <elfakir.med1@gmail.com>
2025-01-08 11:34:23 +0100

    comparison score functionality

--- llmRDLib/cmd_line.py
+++ llmRDLib/cmd_line.py
@@ -6,14 +6,20 @@ def main():
     parser.add_argument('-m', '--model', required=True, help='Le model à utiliser (ex., "llama3").')
     parser.add_argument('-c', '--content', required=True, help='Le contenu du message.')
     parser.add_argument('--host', default='http://0.0.0.0:8000', help='le host du serveur Ollama (default: http://0.0.0.0:8000).')
+    parser.add_argument('--res', type=str, help='Parametre optionnel pour afficher le score d\'une comparaison.')
+
+
 
     args = parser.parse_args()
 
     ollama_client = OllamaChat(host=args.host)
 
     response = ollama_client.chat(model=args.model, content=args.content)
-
-    print(response['message']['content'])
+    if args.res:
+        score = ollama_client.Cmp(desired_output=args.res, generated_output= response['message']['content'])
+        print(f"Score: {round(score*100)}%")
+    else:
+        print(response['message']['content'])
 
 if __name__ == '__main__':
     main()
--- llmRDLib/llm_client.py
+++ llmRDLib/llm_client.py
@@ -1,4 +1,6 @@
 from ollama import Client
+from rouge_score import rouge_scorer
+from sentence_transformers import SentenceTransformer, util
 
 class OllamaChat:
     def __init__(self, host='http://0.0.0.0:8000'):
@@ -16,3 +18,22 @@ class OllamaChat:
         messages = [{'role': 'user', 'content': content}]
         response = self.client.chat(model=model, messages=messages)
         return response
+    def Cmp(self, desired_output, generated_output):
+        
+        scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
+        scores = scorer.score(desired_output, generated_output)
+
+        # Cosine Similarity
+        model = SentenceTransformer('all-MiniLM-L6-v2') 
+        embeddings1 = model.encode(desired_output, convert_to_tensor=True)
+        embeddings2 = model.encode(generated_output, convert_to_tensor=True)
+        cosine_score = util.pytorch_cos_sim(embeddings1, embeddings2).item()
+        # print(f"Cosine Similarity: {cosine_score}")
+        w_rouge_l = 0.15
+        w_cosine = 0.85
+
+        return (w_rouge_l * scores["rougeL"].fmeasure) + (w_cosine * cosine_score)
+
+
+    
+    
